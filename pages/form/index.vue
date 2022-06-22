daa52d97bf0f802a5999ea732c73787ff4fbfeb7
Jaswant Singh <jaswant_s@ch.iitr.ac.in>
2022-06-23 04:51:46 +0530

    Added Recipe Form

--- pages/form/index.vue
+++ pages/form/index.vue
@@ -1,31 +1,126 @@
 <template>
-  <div>
+  <div class="body">
     <Navbar />
     <div class="navbar-spacing"></div>
     <!-- <h1>Form page</h1> -->
 
-    <form v-if="!submitted" ref="form">
+    <form v-if="!submitted" ref="form" @submit.prevent="handleOnSubmit2">
       <div v-if="error" class="error">
         <p v-for="(err, idx) in error" :key="idx">
           {{ err.message }}
         </p>
       </div>
       <div class="form-container">
-        <div class="name">{{ name }}</div>
+        <div class="form-title">Submit Your Recipe</div>
         <div class="description">
-          <p v-for="(line, idx) in textLines2texts(info)" :key="idx">
-            {{ line }}
+          <p>
+            Now you can also submit your own recipe to your favorite cooking
+            site! Share the love of cooking with everyone and let the world
+            taste your magic.
           </p>
         </div>
 
         <!-- Input Fields -->
-        <div v-for="col in cols" :key="col.objKey" class="row--form">
+        <div class="row--form">
           <!-- <h2>[{{ col.title }}]</h2> -->
-          <input :name="col.objKey" type="text" :placeholder="col.title" />
+          <input
+            v-model="form.title"
+            name="title"
+            type="text"
+            placeholder="Recipe Name"
+            required
+          />
+          <input
+            v-model="form.description"
+            name="description"
+            type="text"
+            placeholder="Description"
+            required
+          />
+          <textarea
+            v-model="form.mainContent"
+            name="mainContent"
+            type="text"
+            placeholder="Instructions"
+            rows="30"
+            cols="90"
+            required
+          />
+          <input
+            name="dishImage"
+            type="file"
+            placeholder="Dish Image"
+            @change="uploadFile"
+          />
+          <input
+            v-model="form.timeInMinutes"
+            name="timeInMinutes"
+            type="number"
+            placeholder="Time in Minutes"
+            required
+          />
+          <!-- Input Difficulty -->
+          <div class="difficulty">
+            <input
+              v-model="form.difficulty.label"
+              type="radio"
+              id="easy"
+              name="difficulty"
+              value="Easy"
+            />
+            <label for="easy">Easy</label><br />
+            <input
+              v-model="form.difficulty.label"
+              type="radio"
+              id="medium"
+              name="difficulty"
+              value="Medium"
+            />
+            <label for="medium">Medium</label><br />
+            <input
+              v-model="form.difficulty.label"
+              type="radio"
+              id="hard"
+              name="difficulty"
+              value="Hard"
+            />
+            <label for="hard">Hard</label>
+          </div>
+        </div>
+        <div v-for="i in num_ingredients" class="ingrediens">
+          <input
+            v-model="form.ingredient[i - 1]"
+            :name="'ingredient[' + i + '][ingredient]'"
+            type="text"
+            :placeholder="'ingredient ' + i"
+          />
+          <input
+            v-model="form.amount[i - 1]"
+            :name="'ingredient[' + i + '][amount]'"
+            type="text"
+            placeholder="Amount"
+          />
+        </div>
+        <div class="ingredient-buttons">
+          <button type="button" @click="changeIngredient('+')">Add</button>
+          <button type="button" @click="changeIngredient('-')">Remove</button>
+        </div>
+        <div v-for="i in num_tags" class="tags">
+          <input
+            v-model="form.aditionalTags[i - 1]"
+            :name="'aditionalTags[' + i + ']'"
+            type="text"
+            :placeholder="'Tag ' + i"
+          />
+        </div>
+        <div class="tag-buttons">
+          <button type="button" @click="changeTag('+')">Add</button>
+          <button type="button" @click="changeTag('-')">Remove</button>
         </div>
 
+        <!-- Control Buttons -->
         <div class="row--bottom-next">
-          <button @click="handleOnSubmit">submit</button>
+          <button @click="handleOnSubmit2">submit</button>
           <NuxtLink to="/"> Home </NuxtLink>
         </div>
       </div>
@@ -52,6 +147,8 @@
         <button @click="handleOnBack">back</button>
       </div>
     </form>
+    <div class="footer-spacing"></div>
+    <Footer />
   </div>
 </template>
 
@@ -64,30 +161,85 @@ export default {
       submitted: false,
       submittedId: null,
       error: null,
+      file_id: null,
+      num_ingredients: 1,
+      num_tags: 1,
+      file_id: null,
+      form: {
+        subject: "",
+        contents_type: 18,
+        topics_flg: 1,
+        contents: "",
+        title: "",
+        description: "",
+        mainContent: "",
+        dishImage: {
+          file_id: "",
+          file_nm: "",
+          desc: "",
+        },
+        timeInMinutes: "",
+        difficulty: {
+          key: "",
+          label: "",
+        },
+        ingredient: [""],
+        amount: [""],
+        aditionalTags: [""],
+      },
     };
   },
   methods: {
-    textLines2texts(textLines = "") {
-      return textLines.split("\r\n");
-    },
-    async handleOnSubmit(e) {
-      e.preventDefault();
+    async uploadFile(e) {
+      const fm = new FormData();
+      fm.append("file", e.target.files[0]);
 
-      // collect input elements
-      const formInputElements = Array.from(this.$refs.form.elements).filter(
-        (elm) => elm.tagName.toLowerCase() === "input"
+      const { file_id } = await this.$axios.$post(
+        process.env.BASE_URL + `file`,
+        fm,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data", // required to post file as a binary
+          },
+        }
       );
-
-      // transform key:value inputs to an object
-      const body = formInputElements
-        .map((elm) => ({ [elm.name]: elm.value }))
-        .reduce((prev, cur) => ({ ...prev, ...cur }), {});
-
+      this.form.dishImage.file_id = file_id;
+      console.log("File ID: ", this.form.dishImage.file_id);
+    },
+    async handleOnSubmit2(e) {
+      e.preventDefault();
+      // Additional Processing
+      switch (this.form.difficulty.label) {
+        case "Easy":
+          this.form.difficulty.key = "1";
+          break;
+        case "Medium":
+          this.form.difficulty.key = "2";
+          break;
+        case "Hard":
+          this.form.difficulty.key = "3";
+          break;
+        default:
+          this.form.difficulty.key = "2";
+          this.form.difficulty.label = "Medium";
+          break;
+      }
+      this.form.subject = this.form.title;
+      this.form.timeInMinutes = parseInt(this.form.timeInMinutes);
+      // this.form.ingredient.map((ingredient) => {
+      //   ingredient =
+      //     "{ ingredient: " +
+      //     ingredient.ingredient +
+      //     ", amount: " +
+      //     ingredient.amount +
+      //     "}";
+      // });
+      console.log("Handle Submit Final Form: ", this.form);
       try {
         // post data
         const { id } = await this.$axios.$post(
-          process.env.BASE_URL + `recipe-form?id=${FORM_ID}`,
-          body
+          process.env.BASE_URL + `post-dish`,
+          this.form
         );
         this.error = null;
         this.submittedId = id;
@@ -96,6 +248,55 @@ export default {
         this.error = [`${e}`, ...e.response.data.errors];
       }
     },
+    changeIngredient(mode) {
+      if (mode === "+" && this.num_ingredients < 15) {
+        this.form.ingredient.push("");
+        this.form.amount.push("");
+        this.num_ingredients++;
+      } else if (mode === "-" && this.num_ingredients > 1) {
+        this.num_ingredients--;
+        this.form.ingredient.pop();
+        this.form.amount.pop();
+      }
+    },
+    changeTag(mode) {
+      if (mode === "+" && this.num_tags < 8) {
+        this.form.aditionalTags.push("");
+        this.num_tags++;
+      } else if (mode === "-" && this.num_tags > 1) {
+        this.num_tags--;
+        this.form.aditionalTags.pop();
+      }
+    },
+    textLines2texts(textLines = "") {
+      return textLines.split("\r\n");
+    },
+    // async handleOnSubmit(e) {
+    //   e.preventDefault();
+
+    //   // collect input elements
+    //   const formInputElements = Array.from(this.$refs.form.elements).filter(
+    //     (elm) => elm.tagName.toLowerCase() === "input"
+    //   );
+
+    //   // transform key:value inputs to an object
+    //   const body = formInputElements
+    //     .map((elm) => ({ [elm.name]: elm.value }))
+    //     .reduce((prev, cur) => ({ ...prev, ...cur }), {});
+
+    //   try {
+    //     // post data
+    //     const { id } = await this.$axios.$post(
+    //       process.env.BASE_URL + `recipe-form?id=${FORM_ID}`,
+    //       body
+    //     );
+    //     this.error = null;
+    //     this.submittedId = id;
+    //     this.submitted = true;
+    //   } catch (e) {
+    //     this.error = [`${e}`, ...e.response.data.errors];
+    //   }
+    // },
     handleOnBack(e) {
       e.preventDefault();
       this.submitted = false;
@@ -105,8 +306,13 @@ export default {
 </script>
 
 <style scoped lang="scss">
+.body {
+  position: relative;
+}
+.footer-spacing {
+  height: 150px;
+}
 input {
-  // width: 100%;
   border: none;
   flex-grow: 1;
 }
@@ -116,26 +322,35 @@ input {
 .error {
   color: rgb(223, 87, 87);
 }
+textarea {
+  resize: none;
+}
 
 .form-container {
   display: flex;
   flex-direction: column;
   align-items: stretch;
-  max-width: 700px;
-  margin: 0 auto;
-  padding-inline: 2em;
+  max-width: 650px;
+  margin: 30px auto;
+  padding-inline: 2.5em;
   border-radius: 1em;
   background-color: $primary-color;
   box-shadow: 0 0.5em 1em rgba(0, 0, 0, 0.2);
   font-family: "Poppins", sans-serif;
-  .name {
-    font-size: 1.5em;
-    color: #302939;
+  .form-title {
+    width: 100%;
+    font-size: 2em;
+    font-family: "Poppins", sans-serif;
     font-weight: bold;
+    color: #302939;
+    margin-top: 0.5em;
   }
   .description {
-    font-size: 0.8em;
-    color: #302939;
+    width: 100%;
+    font-size: 1em;
+    color: #625f66;
+    font-family: "Poppins", sans-serif;
+    margin-bottom: 10px;
   }
 }
 
